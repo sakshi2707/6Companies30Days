f9dbe56bdb790addffbbb412cb9d79b7911371dd
Sakshi Gupta <55478753+sakshi2707@users.noreply.github.com>
2022-01-05 13:20:44 +0530

    GoldmanSachs Q12 solution added in java

---
+++ GoldmanSachs12.java
@@ -0,0 +1,33 @@
+//Q12.  Find total number of Squares in a N*N chessboard
+//Solution by Sakshi Gupta
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long N = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.squaresInChessBoard(N));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static Long squaresInChessBoard(Long N) {
+        
+        Long ans = (N*(N+1)*(2*N+1))/6;
+        
+        return ans;
+    }
+};
