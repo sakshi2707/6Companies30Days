062cb5a96003f0b1dc8b8d7ff400901f7118930a
Sakshi Gupta <55478753+sakshi2707@users.noreply.github.com>
2022-01-30 14:46:55 +0530

    Intuit Q2 solution added in java

---
+++ Intuit2.java
@@ -0,0 +1,60 @@
+//Q2.Word Search
+//Solution by Sakshi Gupta
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] S = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(S[0]);
+            int m = Integer.parseInt(S[1]);
+            char [][] board = new char[n][m];
+            for(int i = 0; i < n; i++){
+                String[] s = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++){
+                    board[i][j] = s[j].charAt(0);
+                }
+            }
+            String word = br.readLine().trim();
+            Solution obj = new Solution();
+            boolean ans = obj.isWordExist(board, word);
+            if(ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+       }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+   public boolean isWordExist(char[][] board, String word){char[] w = word.toCharArray();
+    for (int y=0; y<board.length; y++) {
+        for (int x=0; x<board[y].length; x++) {
+            if (exist(board, y, x, w, 0)) return true;
+        }
+    }
+    return false;
+}
+
+private boolean exist(char[][] board, int y, int x, char[] word, int i) {
+    if (i == word.length) return true;
+    if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
+    if (board[y][x] != word[i]) return false;
+    board[y][x] ^= 256;
+    boolean exist = exist(board, y, x+1, word, i+1)
+            || exist(board, y, x-1, word, i+1)
+            || exist(board, y+1, x, word, i+1)
+            || exist(board, y-1, x, word, i+1);
+    board[y][x] ^= 256;
+    return exist;
+}
+}
